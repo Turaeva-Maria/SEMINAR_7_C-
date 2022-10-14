7ce09423fce6861cca40d236ce1d346f07f33d9b
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-10-14 22:20:33 +0300

    Программа на двумерный массив, поиск обоих четных индексов в нем и их замену на их квадраты

---
+++ task_49/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты.
+Например, изначально массив выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Новый массив будет выглядеть вот так:
+1 4 7 2
+5 81 2 9
+8 4 2 4 
+*/
+
+Console.Clear();
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + " \t");
+        Console.WriteLine();
+    }
+}
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 1 && j % 2 == 1)
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+}
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
+PrintMatrix(matrix);
